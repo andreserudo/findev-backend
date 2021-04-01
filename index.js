f2a622593b6a8a6e9f4616285e497b65018e040a
Andre Serudo <andreserudo@gmail.com>
2021-04-01 12:04:53 -0400

    fix allow origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://findev-backend-serudo.herokuapp.com");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');    
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
