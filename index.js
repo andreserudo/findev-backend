b285fa131ad61c0247b8e2107652af87eabcd5da
Andre Serudo <andreserudo@gmail.com>
2021-04-01 11:42:04 -0400

    habilitando cors

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use((_req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use('/candidates', candidates);
 
 app.listen(PORT, () => {
