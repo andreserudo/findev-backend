594801c86b337249ecfe9990486bd006a91e4a9e
Andre Serudo <andreserudo@gmail.com>
2021-04-01 11:04:36 -0400

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const candidates = require('./routes/candidates.js');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/candidates', candidates);
 
 app.listen(PORT, () => {
